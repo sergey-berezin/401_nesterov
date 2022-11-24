dc4ac6f5b6564a75bd334f15eacb6e03fadcfe84
Dmitriy Nesterov <dim.nesterov2015@gmail.com>
2022-11-24 09:29:08 +0300

    WindowApp: added persistent database on SQLite
    Added database on Entity Framework core based on SQLite engine,
    images are stored as byte arrays with corresponding embeddings.
    Image retrieval is done by first filtering with hash function,
    then is compared all byte data.

---
+++ WindowApp/StorageWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WindowApp
+{
+    /// <summary>
+    /// Логика взаимодействия для StorageWindow.xaml
+    /// </summary>
+    public partial class StorageWindow : Window
+    {
+        public ObservableCollection<Image> Images { get; private set; }
+        public StorageWindow()
+        {
+            Images = new ObservableCollection<Image>();
+
+            using (var db = new ImagesContext())
+            {
+                foreach (var image in db.Images)
+                    Images.Add(image);
+            }
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ButtonDelete(object sender, RoutedEventArgs e)
+        {
+            if (!Images.Any())
+                return;
+
+            try
+            {
+                var image = Images[ImagesListBox.SelectedIndex];
+                using (var db = new ImagesContext())
+                {
+                    var deletedImage = db.Images
+                        .Where(x => x.Id == image.Id)
+                        .Include(x => x.Details)
+                        .First();
+
+                    if (deletedImage == null)
+                        return;
+
+                    db.Details.Remove(deletedImage.Details);
+                    db.Images.Remove(deletedImage);
+                    db.SaveChanges();
+                }
+                Images.Remove(image);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ButtonClear(object sender, RoutedEventArgs e)
+        {
+            if (!Images.Any())
+                return;
+
+            using (var db = new ImagesContext())
+            {
+                db.Clear();
+                db.SaveChanges();
+            }
+
+            Images.Clear();
+        }
+    }
+}
